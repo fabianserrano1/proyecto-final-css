8e4d5972265c0acb9789edb13898fd94c9f38843
fabianserrano1 <fabianserrano1@gmail.com>
2026-03-14 14:13:49 +0100

    Primer commit proyecto final CSS

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { resolve } from 'path'
+
+// Obtener __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = resolve(__filename, '..')
+
+// Detectar si estamos en producción
+const isProduction = process.env.NODE_ENV === 'production'
+
+// Exportar configuración
+export default defineConfig({
+  base: '/Proyecto-final-css/', // esto asegura que GitHub Pages encuentre assets
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        contacto: resolve(__dirname, 'contacto.html'),
+        cursos: resolve(__dirname, 'cursos.html'),
+        login: resolve(__dirname, 'login.html'),
+        registro: resolve(__dirname, 'registro.html'),
+        quienes_somos: resolve(__dirname, 'quienes_somos.html'),
+        aviso_legal: resolve(__dirname, 'aviso_legal.html'),
+        privacidad: resolve(__dirname, 'privacidad.html'),
+        blog1: resolve(__dirname, 'blog/Desarrollo_Full_Stack_para_perfiles_Junior.html'),
+        blog2: resolve(__dirname, 'blog/El_futuro_brillante_del_desarrollo_Full_Stack_en_España.html')
+      },
+    },
+  },
+});
