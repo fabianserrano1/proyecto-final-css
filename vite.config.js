9a12866044d60bda6f88da71ea5d7b7c62a86c17
fabianserrano1 <fabianserrano1@gmail.com>
2026-03-15 02:35:12 +0100

    fix github pages paths

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 // Exportar configuración
 export default defineConfig({
-  base: '/Proyecto-final-css/', // esto asegura que GitHub Pages encuentre assets
+  base: '/proyecto-final-css/', 
   root: '.',
   build: {
     outDir: 'dist',
@@ -27,6 +27,8 @@ export default defineConfig({
         quienes_somos: resolve(__dirname, 'quienes_somos.html'),
         aviso_legal: resolve(__dirname, 'aviso_legal.html'),
         privacidad: resolve(__dirname, 'privacidad.html'),
+        full_stack: resolve(__dirname, 'cursos/full_stack.html'),      
+        blockchain: resolve(__dirname, 'cursos/blockchain.html'),
         blog1: resolve(__dirname, 'blog/Desarrollo_Full_Stack_para_perfiles_Junior.html'),
         blog2: resolve(__dirname, 'blog/El_futuro_brillante_del_desarrollo_Full_Stack_en_España.html')
       },
